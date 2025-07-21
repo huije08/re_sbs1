b37a31b5bd7c7b5630c647fb6512a93f7c0609ee
huije08 <huijegang@gmail.com>
2025-07-21 19:47:22 +0900

    (2025.7.21)조건문

---
+++ Class4th (Overflow & Underflow)/Source.c
@@ -0,0 +1,55 @@
+﻿#include <stdio.h>
+
+void main()
+{
+#pragma region 오버플로우
+	// 특정한 자료형이 표현할 수 있는 최댓값의 버위를 
+	// 넘어서 연산을 수행하는 과정입니다.
+
+	// 오버플로우 부호 없는 자료형에서도 똑같이 발생하며, 
+	// 실수일 떄 오버플로우가 발생하면 infinity라는 값으로 출력됩니다.
+
+#pragma endregion
+
+#pragma region 언더플로우
+	// 특정한 자료형이 표현할 수 있는 최솟값의 범위를 
+	// 넘어서 연산을 수행하는 과정입니다.
+
+	//char data = -128 - 1;
+
+
+
+	// 언더플로우는 부호없는 자료형에서도 똑같이 발생하며, 
+	// 실수일 때 언더플로우가 발생하면 0이라는 값으로 출력됩니다.
+#pragma endregion
+
+#pragma region 부호 없는 자료형
+	//unsigned int count = 4294967295;
+
+
+	//printf("%d \n", count);
+
+
+	//printf("%u \n", count);
+#pragma endregion
+
+#pragma region 실수를 표현하는 방법
+
+	// float decimal = 3.1f;
+	// double x = 3.1;
+
+	// 정수형 리터럴 상수는 저장하는 값의 범위가 크지 않으면
+	// int 자료형으로 저장되며, 실수형 리터럴 상수는 double
+	// 자료형으로 저장됩니다.
+
+	// printf("decimal 변수의 값 : %f\n", decimal);
+	// printf("x 변수의 값 : %.16lf\n", x);
+	// 표준 출력 함수로 소수를 출력할 때 4byte 크기의 실수형
+	// 자료형은 소수점 이하 6자리 까지만 표기하며, 8byte 크기의
+	// 실수형 자료형은 소수점 이하 15자리 까지만 표기합니다.
+#pragma endregion
+
+
+
+	return 0;
+}
--- Program/Source.c
+++ Program/Source.c
@@ -2,35 +2,105 @@
 
 void main()
 {
-#pragma region 오버플로우
-	// 특정한 자료형이 표현할 수 있는 최댓값의 버위를 
-	// 넘어서 연산을 수행하는 과정입니다.
+#pragma region 조건문
+	// 어떤 조건이 주어질 때 해당 조건에 따라 동작을
+	// 수행하도록 실행하는 명령문입니다.
 
-	// 오버플로우 부호 없는 자료형에서도 똑같이 발생하며, 
-	// 실수일 떄 오버플로우가 발생하면 infinity라는 값으로 출력됩니다.
+#pragma region 관계 연산자
+	// 두 개의 피연산자의 값을 비교하여 그 결과를 0 또는
+	// 1이라는 값으로 나타내는 연산자입니다.
+
+	// int positionX=10;
+	// int positionY=20;
+	// 
+	// printf("positionX < positionY : %d\n", positionX < positionY);
+	// printf("positionX > positionY : %d\n", positionX > positionY);
+	// printf("positionX <= positionY : %d\n", positionX <= positionY);
+	// printf("positionX >= positionY : %d\n", positionX >= positionY);
+	// printf("positionX != positionY : %d\n", positionX != positionY);
+	// printf("positionX == positionY : %d\n", positionX == positionY);
+
+	// 관계 연산자는 조건이 맞을 때 1이라는 값으로 반환되며,
+	// 조건이 틀릴 떄 0이라는 값으로 반환됩니다.
 
 #pragma endregion
 
-#pragma region 언더플로우
-	// 특정한 자료형이 표현할 수 있는 최솟값의 범위를 
-	// 넘어서 연산을 수행하는 과정입니다.
+#pragma region if문
+	// 어떤 특정한 조건을 비교하여 조건이 맞다면 실행되는
+	// 명령문입니다.
 
-	//char data = -128 - 1;
+	//int health = 0;
 
-
-
-	// 언더플로우는 부호없는 자료형에서도 똑같이 발생하며, 
-	// 실수일 때 언더플로우가 발생하면 0이라는 값으로 출력됩니다.
+	//if (health == 0)
+	//{
+	//	printf("xzczxc");
+	//}
 #pragma endregion
 
-#pragma region 부호 없는 자료형
-	unsigned int count = 4294967295;
+#pragma region else if 문
+	// if문의 조건이 틀릴 때 else if문의 조건이
+	// 맞다면 실행되는 명령문 입니다.
+
+	//int level = 9;
+	//
+	//printf(" Your level : %d\n", level);
+	//
+	//if (level == 99)
+	//{
+	//	printf("최대레벨 입니다! \n");
+	//}
+	//else if (level < 99)
+	//{
+	//	printf("아직 최대레벨이 아닙니다.\n");
+	//	printf("열심히 레벨업 해보세요!\n");
+	//}
+	
+#pragma endregion
+
+#pragma region else문
+	// if문과 else if문의 조건이 다 틀리면
+	// 실행되는 명령문입니다.
+	//int health = 10;
+	//if (health > 0)
+	//{
+	//	printf("alive");
+	//}
+	//else if (0 < health <= 10)
+	//{
+	//	printf("위험합니다 회복하세요");
+	//}
+	//else
+	//{
+	//	printf("dead");
+	//}
+
+#pragma endregion
+
+#pragma region switch문
+	// 어떤 결과에 따라 그 결과부터 실행되는 명령문 입니다.
+
+	char grade = 'B';
+
+	switch (grade)
+	{
+		case 'A': printf("100~90점 입니다. \n");
+			break;
+		case 'B': printf("89~80점 입니다. \n");
+			break;
+		case 'C': printf("79~70점 입니다. \n");
+			break;
+		case 'D': printf("69~60점 입니다. \n");
+			break;
+		case 'F': printf("60점 이하 입니다. \n");
+			break;
+
+		default: printf("Exception\n");
+			break;
+		
+	}
+#pragma endregion
 
 
-	printf("%d \n", count);
-
-
-	printf("%u \n", count);
 #pragma endregion
 
 
